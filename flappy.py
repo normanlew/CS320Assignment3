a0431d11984b01b9c6a9a241d88bed789f06b40a
Norman Lew <normanlew@users.noreply.github.com>
2018-03-01 18:31:43 -0800

    Add files via upload
    This is the program Marius and I are turning in for this assignment

--- flappy.py
+++ flappy.py
@@ -1,4 +1,4 @@
-# Norman Lew
+# Norman Lew & Marius Popescu
 # CS 320
 # Winter 2018
 
@@ -16,13 +16,19 @@ import random
 
 pygame.init()
 
+# get sound
+crash_sound = pygame.mixer.Sound("crash.wav")  
+pygame.mixer.music.load("bird.wav")
+
 display_width = 500
 display_height = 400
 
 black = (0,0,0)
 white = (255,255,255)
-red = (255,0,0)
+red = (200,0,0)
+bright_red = (255,0,0)
 green = (0,255,0)
+bright_green = (0,255,0)
 
 bird_width = 40
 bird_height = 32
@@ -32,6 +38,14 @@ pygame.display.set_caption('Flappy bird')
 clock = pygame.time.Clock()
 
 birdImg = pygame.image.load('forwardbird3.png')
+
+pause = False # define and set pause to false
+
+# This method will count the number of pipes passed and keep the score
+def pipe_score(count):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("SCORE: "+str(count), True, black)
+    gameDisplay.blit(text,(0,0))
 
 # Function that draws the pipe to the screen
 def pipe(pipeX, pipeY, pipeW, pipeH, color):
@@ -53,44 +67,125 @@ def message_display(text):
     TextRect.center = ((display_width/2), (display_height/2))
     gameDisplay.blit(TextSurf, TextRect)
 
-    pygame.display.update()
+#    pygame.display.update()
     
 # Game is over
 def die():
+    # set the music
+    pygame.mixer.music.stop()
+    pygame.mixer.Sound.play(crash_sound)
+    
     message_display('Game Over')
+    while True :
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+              quitgame()
+        # Call button method
+        button("Play Again!", 100, 300, 150, 50, green, bright_green, game_loop)
+        button("Quit", 300, 300, 100, 50, red, bright_red, quitgame)
+        pygame.display.update()
+        clock.tick(15)
+
+#define the start button method
+#the method pass a message, x and y coordinates, width, height, a inactive collor, a active collor and a action/method
+def button(msg,x,y,w,h,ic,ac, action=None):
+    mouse = pygame.mouse.get_pos() # Grab mouse position
+    click = pygame.mouse.get_pressed() # Get the click
+    # Create the start and quit buttons
+    if x+w > mouse[0] > x and y+h > mouse[1] > y: # If the mouse cursor is over the button
+        pygame.draw.rect(gameDisplay, ac, (x, y, w, h)) # Color the mouse lighter to look interactive
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(gameDisplay, ic, (x, y, w, h))
+    smallText = pygame.font.Font('freesansbold.ttf', 20)
+    TextSurf, TextRect = text_objects(msg, smallText)   #set the tesx and the font and type of text inside the rectangle where will be the text 
+    TextRect.center = ((x+(w/2)), (y+(h/2))) #set this mesage in the center of screen
+    gameDisplay.blit(TextSurf, TextRect)
+
+# Define the quit game method
+def quitgame():
+    pygame.quit()
+    quit()
+
+
+# The game intro method
+def game_intro():
+    intro = True
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quitgame()
+
+        gameDisplay.fill(white)
+        # display the message on the screen
+        message_display('Flappy Bird')
+        # Call button method
+        button("PLAY!", 100, 300, 100, 50, green, bright_green, game_loop)
+        button("Quit", 300, 300, 100, 50, red, bright_red, quitgame)
+       
+        pygame.display.update()
+        clock.tick(15)
+
+# The unpause method
+def unpause():
+    global pause
+    pygame.mixer.music.unpause()
+    pause = False
+    
+
+# Define the game pause method
+def paused():
+    pygame.mixer.music.pause() # stop the music
+    
+    message_display('Paused')
+    while pause:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quitgame()
+ 
+        # Call button method
+        button("Continue", 100, 300, 100, 50, green, bright_green, unpause)
+        button("Quit", 300, 300, 100, 50, red, bright_red, quitgame)
+       
+        pygame.display.update()
+        clock.tick(15)       
 
 # The main game loop
 def game_loop():
-    x_bird = (display_width * 0.3)
+
+     # set the music
+    pygame.mixer.Sound.stop(crash_sound)
+    pygame.mixer.music.play(-1) # -1 will play the music in a loop
+    global pause
+    
+    x_bird = (display_width * 0.05)
     y_bird = (display_height * 0.4)
 
-    score = 0
+    x_bird_change = 0
+    y_bird_change = 0  
 
     # This is the first set of pipes.  A set of pipes consists of a pipe protruding from the top of the screen and another one protruding from the bottom of the screen
     pipe_x = display_width
-    pipe_y = random.randrange(115, display_height)
+    pipe_y = random.randrange(195, display_height)
     pipe_speed = 2
     pipe_width = 60
     pipe_height = display_height - pipe_y
 
     pipe_x2 = display_width
     pipe_y2 = 0
-    pipe_height2 = pipe_y - 100
-    
-
+    pipe_height2 = pipe_y - 200 
 
     # This is the second set of pipes.
     pipe_x3 = display_width + 275
-    pipe_y3 = random.randrange(115, display_height)
+    pipe_y3 = random.randrange(195, display_height)
     pipe_height3 = display_height - pipe_y3
 
     pipe_x4 = display_width + 275
     pipe_y4 = 0
-    pipe_height4 = pipe_y3 - 100
+    pipe_height4 = pipe_y3 - 200
 
-
-    x_bird_change = 0
-    y_bird_change = 0
+    score = 0 
 
     gameExit = False
 
@@ -98,14 +193,16 @@ def game_loop():
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+                quitgame()
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    y_bird_change = -11
+                    y_bird_change = -5
+                if event.key == pygame.K_p: # if 'p' is pressed then pause the game
+                    pause = True
+                    paused()  # call the paused method
             if event.type == pygame.MOUSEBUTTONDOWN:
-                y_bird_change = -11
+                y_bird_change = -5
                 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_SPACE:
@@ -129,7 +226,9 @@ def game_loop():
 
             pipe_x2 = display_width
             pipe_y2 = 0
-            pipe_height2 = pipe_y - 100
+            pipe_height2 = pipe_y - (200 - 3*score) #narrow the hole in the pipe
+            score += 1 # increase the score
+            pipe_speed += score*0.03 # increase the speed 
 
         if (pipe_x3 < (0 - pipe_width)) :
             pipe_x3 = display_width
@@ -138,8 +237,9 @@ def game_loop():
 
             pipe_x4 = display_width
             pipe_y4 = 0
-            pipe_height4 = pipe_y3 - 100
-
+            pipe_height4 = pipe_y3 - (200 - 3*score) #narrow the hole in the pipe
+            score += 1 # increase the score
+            pipe_speed += score*0.03 # increase the speed
         
         # Update the location of the pipes and the bird
         pipe_x -= pipe_speed
@@ -152,8 +252,10 @@ def game_loop():
         pipe(pipe_x3, pipe_y3, pipe_width, pipe_height3, green)
         pipe_x4 -= pipe_speed
         pipe(pipe_x4, pipe_y4, pipe_width, pipe_height4, green)
-        
+
+        # show the bird
         bird(x_bird,y_bird)
+        pipe_score(score)
 
         pygame.display.update()
 
@@ -165,6 +267,8 @@ def game_loop():
         
         clock.tick(60)
 
+#call the intro
+game_intro()
+#call the game loop
 game_loop()
-pygame.quit()
-quit()
+quitgame()
